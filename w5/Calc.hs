eb422ae5341caf00dc4ec79e33191533c070084b
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-06-05 14:00:00 +0200

    adding files

--- w5/Calc.hs
+++ w5/Calc.hs
@@ -1,6 +1,17 @@
 import ExprT
+import Parser
 
+-- evaluate expression into integer
 eval :: ExprT -> Integer
 eval (Lit x)   = x
 eval (Add x y) = eval x + eval y
-eval (Mul x y) = eval x * eval y 
+eval (Mul x y) = eval x * eval y
+
+-- evaluate string into maybe string
+evalStr :: String -> Maybe ExprT
+evalStr = parseExp Lit Add Mul
+
+instance ExprT Integer where
+  lit :: Integer -> ExprT
+  mul :: 
+  add :: 
