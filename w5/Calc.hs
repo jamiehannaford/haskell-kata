928a15dcafbdedb952a33dbd62f0d3755958446c
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-06-08 19:41:57 +0200

    further exercises

--- w5/Calc.hs
+++ w5/Calc.hs
@@ -22,9 +22,33 @@ instance Expr ExprT where
   add x y = Add x y
 
 instance Expr Integer where
-  lit x = Lit x
-  mul x y = Mul x y
-  add x y = Add x y
+  lit x = x
+  mul x y = x * y
+  add x y = x + y
 
-reify :: ExprT -> ExprT
-reify = id
+instance Expr Bool where
+  lit x = if x <= 0 then False else True
+  mul x y = x || y
+  add x y = x && y
+
+newtype MinMax = MinMax Integer deriving (Eq, Show)
+
+instance Expr MinMax where
+  lit x = MinMax x
+  add (MinMax x) (MinMax y) = MinMax (max x y)
+  mul (MinMax x) (MinMax y) = MinMax (min x y)
+
+newtype Mod7 = Mod7 Integer deriving (Eq, Show)
+
+instance Expr Mod7 where
+  lit x = Mod7 (x `mod` 7)
+  add (Mod7 x) (Mod7 y) = Mod7 ((x+y) `mod` 7)
+  mul (Mod7 x) (Mod7 y) = Mod7 ((x*y) `mod` 7)
+
+testExp :: Expr a => Maybe a
+testExp = parseExp lit add mul ("(3 * -4) + 5")
+
+testInteger = testExp :: Maybe Integer
+testBool = testExp :: Maybe Bool
+testMM = testExp :: Maybe MinMax
+testSat = testExp :: Maybe Mod7
