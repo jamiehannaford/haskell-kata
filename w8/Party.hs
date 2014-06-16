1a312c1bedc53cd7af129d559f32f97754086a48
Jamie Hannaford <jamie.hannaford@rackspace.com>
2014-06-16 19:33:00 +0200

    fun fun fun

--- w8/Party.hs
+++ w8/Party.hs
@@ -2,4 +2,14 @@ module Party where
 
 import Employee
 
+import Data.Monoid
+
 glCons :: Employee -> GuestList -> GuestList
+glCons e@(Emp { empFun = f1 }) (GL es f) = GL (es ++ [e]) (f+f1)
+
+instance Monoid GuestList where
+  mempty  = GL [] 0
+  mappend l1@(GL es1 f1) l2@(GL es2 f2) = GL (es1 ++ es2) (f1 + f2)
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun g1@(GL _ f1) g2@(GL _ f2) = if f1 >= f2 then g1 else g2
